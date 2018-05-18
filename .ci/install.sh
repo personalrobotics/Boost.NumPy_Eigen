1fa4659cc177478f8b10c063671a90336faccd20
Jeongseok Lee <jslee02@gmail.com>
2018-05-18 08:58:25 -0700

    Fix syntax error

--- .ci/install.sh
+++ .ci/install.sh
@@ -7,6 +7,6 @@ sudo apt-get -qq update
 sudo apt-get install -y \
   libeigen3-dev \
   libboost-python-dev \
-  libboost-python-numpy-dev \ # from ppa:personalrobotics/ppa
+  libboost-python-numpy-dev \
   python3-numpy \
   libpython3-dev \
